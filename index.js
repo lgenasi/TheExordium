cb3b62fb16bb11054041c4244ecb75fc6b50cd5b
lgenasi <louis.genasi@gmail.com>
2015-04-25 10:36:51 +0100

    Installed packages: {bower, express, http} and added Bootstrap and jquery via bower. Hopefully initialised a localhost version of a test page @ port 5000.

---
+++ index.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var express = require('express');
+
+var pkg = require('./package');
+var route = pkg.route;
+var port = pkg.port;
+
+var exec = require('child_process').exec;
+
+var app = express();
+app.use("/src", express.static(__dirname + '/src'));
+app.use("/lib", express.static(__dirname + '/bower_components'));
+var server = http.createServer(app);
+
+app.set('port', (process.env.PORT || 5000))
+
+app.get(route, function (req, res) {
+  res.sendfile('main.html');
+});
+
+server.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
