20375ec1e3b9fe540be14c00879248d5b7bd5a92
lgenasi <louis.genasi@gmail.com>
2015-06-13 12:42:33 +0100

    removed the need for the forum iframe after fixing the template and theme of the forum to match ours

--- forum.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	var goToCalendar = localStorage.getItem("goToCalendar");
-	if(goToCalendar){
-		$("#iframe").attr("src", "http://www.theexordium.org/phpbb3/dkp.php?page=planner");
-		localStorage.removeItem("goToCalendar");
-	}
-});
