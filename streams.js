6545e4541653fe3d071a0fe9e1981de69787859e
lgenasi <louis.genasi@gmail.com>
2015-06-14 02:01:11 +0100

    converted streamer list to buttons which only appear for active streamers. also added a refresh button which will re-populate the buttons based on a new request for active streamers. not yet any proper handling if there are no streamers active (currently you just won't see any buttons).

--- streams.js
+++ streams.js
@@ -1,55 +1,45 @@
 $(document).ready(function(){
-
-	var streamers = new Array();
-
-	$.ajax({
-		method: "GET",
-		url: "http://www.theexordium.org:1935/statsXMLCORS?flat",
-		dataType: "xml",
-		async: false
-	})
-	.done(function(xml){
-		$(xml).find('Stream').each(function(){
-			var $entry = $(this);
-			var streamer = $entry.attr('streamName');
-			streamers.push(streamer);
+	function getActiveStreamers(){
+		$.ajax({
+			method: "GET",
+			url: "http://www.theexordium.org:1935/statsXMLCORS?flat",
+			dataType: "xml",
+			async: false
 		})
-	});
-	
-	var streamerList = document.getElementById("selectStreamer"); 
+		.done(function(xml){
+			$('#streamerList').empty();
+			$(xml).find('Stream').each(function(){
+				var $entry = $(this);
+				var streamer = $entry.attr('streamName');
+				generateBtn(streamer);
+			})
+		})
+	}
 
-	for(var i = 0; i < streamers.length; i++) {
-
-		var opt = streamers[i];
-		var el = document.createElement("option");
+	function generateBtn(streamer){
+		var opt = streamer;
+		var el = document.createElement("button");
 		el.textContent = opt;
 		el.value = opt;
-		streamerList.appendChild(el);
+		el.className = "streamerBtn";
+		$('#streamerList').append(el);
 	}
+
+	$(document).on('click', ".streamerBtn", function() {
+		$("#active").remove();
+		$('.stream-player').prepend(
+			'<object id="active" width="925" height="600">'+
+			'	<param name="movie" value="http://fpdownload.adobe.com/strobe/FlashMediaPlayback.swf">'+
+			'	<param name="flashvars" value="src=rtmp%3a%2f%2ftheexordium.org%3a1935%2flive%2f' + this.value + '&amp;poster=http%3A%2F%2Fosmf.org%2Fimages%2Fposter_cathy_fmp.jpg&amp;streamType=live">'+
+			'	<param name="allowFullScreen" value="true">'+
+			'	<param name="allowscriptaccess" value="always">'+
+			'	<embed src="http://fpdownload.adobe.com/strobe/FlashMediaPlayback.swf" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="500" height="300" flashvars="src=rtmp%3A%2F%2Ftheexordium.org%3A1935%2Flive%2FTom&amp;poster=http%3A%2F%2Fosmf.org%2Fimages%2Fposter_cathy_fmp.jpg&amp;streamType=live">'+
+			'</object>').hide().fadeIn(600);	    
+	});
 	
-	$("#selectStreamer").on("change", function(){
-		
-		var selectedStreamer;
-		for(var i = 0; i < streamerList.length; i++) {	
-
-			if (streamerList[i].selected){
-				selectedStreamer = streamerList[i].value;
-				for(var j = 0; j < streamers.length; j++) {
-					if (selectedStreamer == streamers[j]){
-						$("#active").remove();
-						$('.stream-player').prepend(
-							'<object id="active" width="925" height="600">'+
-							'	<param name="movie" value="http://fpdownload.adobe.com/strobe/FlashMediaPlayback.swf">'+
-							'	<param name="flashvars" value="src=rtmp%3a%2f%2ftheexordium.org%3a1935%2flive%2f' + streamers[j] + '&amp;poster=http%3A%2F%2Fosmf.org%2Fimages%2Fposter_cathy_fmp.jpg&amp;streamType=live">'+
-							'	<param name="allowFullScreen" value="true">'+
-							'	<param name="allowscriptaccess" value="always">'+
-							'	<embed src="http://fpdownload.adobe.com/strobe/FlashMediaPlayback.swf" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="500" height="300" flashvars="src=rtmp%3A%2F%2Ftheexordium.org%3A1935%2Flive%2FTom&amp;poster=http%3A%2F%2Fosmf.org%2Fimages%2Fposter_cathy_fmp.jpg&amp;streamType=live">'+
-							'</object>').hide().fadeIn(600);				
-					}
-				}
-			}
-
-		}		
+	$("#refreshBtn").click(function(){
+		getActiveStreamers();
 	});
 
+	getActiveStreamers();
 });
